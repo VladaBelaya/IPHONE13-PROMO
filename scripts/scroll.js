7d2df0b77b832a8a4ebe8f81622ef3dc646425c8
VladaBelaya <vlada_chvanova@mail.ru>
2022-01-22 13:19:10 +0300

    finished with tabs

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,25 +1,29 @@
-const links = document.querySelectorAll('.header-menu__item a')
-const details = document.querySelector('.card-details__link-characteristics')
-const arrayWithLinks = [...links, details]
+const scrollFunc = () => {
+    const links = document.querySelectorAll('.header-menu__item a')
+    const details = document.querySelector('.card-details__link-characteristics')
+    const arrayWithLinks = [...links, details]
 
-seamless.polyfill()
+    seamless.polyfill()
 
-arrayWithLinks.forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault()
+    arrayWithLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault()
 
-        const id = link.getAttribute('href').substring(1)
-        const section = document.getElementById(id)
-        if (section) {
-            seamless.elementScrollIntoView(section, {
-                behavior: 'smooth',
-                block: 'start'
-            })
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-                behavior: "smooth",
-                block: "start",
-            })
-        }
+            const id = link.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "start",
+                })
+            }
+        })
     })
-})
+}
+
+scrollFunc()
