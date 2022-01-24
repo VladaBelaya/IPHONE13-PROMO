56ca1caed79fc15e3922109652fd5096b0b9e33a
VladaBelaya <vlada_chvanova@mail.ru>
2022-01-24 13:20:02 +0300

    finished lesson "accordion"

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,5 +1,18 @@
 const getData = () => {
-
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('Данные были получены с ошибкой')
+            }
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error(error.message)
+        })
 }
 
 getData()
